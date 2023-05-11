3f37834d70866afc69172dfcb8d751a511e4f054
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2023-05-11 16:14:09 -0400

    Update lubuntu-to-chrome.sh

--- lubuntu-to-chrome.sh
+++ lubuntu-to-chrome.sh
@@ -35,7 +35,7 @@ sudo apt-get remove kde* -y
 sudo apt-get remove gnome* -y
 
 # Eliminamos programas
-sudo apt remove firefox sylpheed pidgin gpicview audacious mtpaint evince -y
+sudo apt remove firefox sylpheed pidgin-data audacious evince-common -y
 # sudo apt remove pulseaudio mpv leafpad vim -y (rompe el sistema)
 
 # Purgamos
