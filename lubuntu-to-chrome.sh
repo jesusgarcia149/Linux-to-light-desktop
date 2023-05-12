979e47e24af1d15e8448c199487e78184e66cc88
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2023-05-12 09:40:03 -0400

    Update lubuntu-to-chrome.sh

--- lubuntu-to-chrome.sh
+++ lubuntu-to-chrome.sh
@@ -67,7 +67,7 @@ sudo apt install google-chrome-stable -y
 
 # Eliminamos los programas
 sudo apt remove firefox sylpheed pidgin* audacious evince* -y
-sudo apt remove pulseaudio leafpad vim* fcitx* hardinfo htop system-config-printer synaptic -y
+sudo apt remove leafpad vim* fcitx* hardinfo htop system-config-printer synaptic -y
 sudo apt remove abiword* gnumeric* mtpaint* simple-scan* gpicview* transmission* xfburn* guvcview* xpad* galculator file-roller* -y
 
 # Eliminamos las fuentes
